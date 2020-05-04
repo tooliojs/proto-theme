95f6d1305476b1a5c99b722d25a70746cb7aa79e
arakilian0 <arakilian0@gmail.com>
2020-05-03 17:07:40 -0700

    UPDATED theme builder - functioning prototype

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 const fs = require('fs')
 const path = require('path')
 const os = require('os').platform()
+const mkcp = require('mkcp')
+const clean = require('js-beautify')
+const option = {
+    html: {
+        indent_size: 2
+    }
+}
 
 let resolved = []
 let marker; os === 'win32' ? marker = '\\' : marker = '/'
+let nav_input
+let nav_output
+let nav_open = `<nav class="proto-header--body"><ul>`
+let nav_close = `</ul></nav>`
 
 const rmrf = function(ipath) {
     if (fs.existsSync(ipath)) {
@@ -27,11 +38,12 @@ function orderError(code) {
     }
 }
 
-function template(content) {
+function template(nav, content) {
     let prefix = path.join(__dirname, '..', 'public', 'template')
     let head = fs.readFileSync(path.join(prefix, 'head.html'), { encoding:'utf8', flag:'r' })
     let body = fs.readFileSync(path.join(prefix, 'body.html'), { encoding:'utf8', flag:'r' })
-    return head + content + body
+    let header = fs.readFileSync(path.join(prefix, 'header.html'), { encoding:'utf8', flag:'r' })
+    return head + '\n' + header + nav + '</header><main class="proto-markdown">\n' + content + '</main>\n' + body
 }
 
 module.exports = function(options, files) {
@@ -42,11 +54,21 @@ module.exports = function(options, files) {
 
     resolved.forEach(file => {
         let filename = file.split(marker)[file.split(marker).length - 1]
+        if(!nav_input) nav_input = `<li><a href="${filename}">${filename.split('.html')[0]}</a></li>`
+        else nav_input += `<li><a href="${filename}">${filename.split('.html')[0]}</a></li>`
+    })
+
+    nav_output = nav_open + nav_input + nav_close
+
+    resolved.forEach(file => {
+        let filename = file.split(marker)[file.split(marker).length - 1]
         let snippet = fs.readFileSync(file, { encoding:'utf8', flag:'r' })
-        fs.writeFileSync(path.resolve(options.output, filename), template(snippet))
+        fs.writeFileSync(path.resolve(options.output, filename), clean.html(template(nav_output, snippet, option.html)))
     })
 
     rmrf(path.resolve(options.tmp))
+    mkcp(path.join(__dirname, '..', 'public', 'css'), path.resolve(options.output, 'css'))
+    mkcp(path.join(__dirname, '..', 'public', 'js'), path.resolve(options.output, 'js'))
 
     return 0
 }
